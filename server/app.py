c25e1d8397c1e44eac3c82ff9f2f474a59146302
IanGichane <gichaneian@gmail.com>
2024-01-16 14:24:40 +0300

    Flask lab

--- server/app.py
+++ server/app.py
@@ -15,18 +15,42 @@ db.init_app(app)
 
 @app.route('/')
 def home():
+       
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id==id).first()
+    if not animal:
+        response_body = '<h1>404 Animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <h1>ID {animal.id}</h1>
+        <h1>Name {animal.name}</h1>
+        <h1>Species {animal.species}</h1>
+        <h1>Zookeeper {animal.zookeeper}</h1>
+        <h1>Enclosure {animal.enclosure}</h1>
+        
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
+    # zookeeper = Zookeeper.query.filter(Zookeeper.id==id).first()
+    # if not zookeeper:
+
     return ''
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
+    # enclosure = Enclosure.query.filter(Enclosure.id==id).first()
+    # if not enclosure:
+
     return ''
 
 
--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -11,13 +12,26 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
+    birthday = db.Column(db.String(20), nullable=False)
+    animal = db.relationship('Animal', backref='zookeepers', lazy=True)
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
+    environment  = db.Column(db.String(50), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, nullable=False)
+    enclosed_animals = db.relationship('Animal', backref='enclosures', lazy=True)
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
+    species = db.Column(db.String(20), nullable=False)
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'), nullable=False)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'), nullable=False)
+    enclosure = db.relationship('Enclosure', backref='animals', lazy=True)
+    zookeeper = db.relationship('Zookeeper', backref='animals', lazy=True)  
